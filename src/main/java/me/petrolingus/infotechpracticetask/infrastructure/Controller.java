dd3a0a95fa3d8e09e075955d89197a767b62824e
petrolingus <petrolingus@gmail.com>
2021-04-06 02:35:40 +0300

    created business logic

--- src/main/java/me/petrolingus/infotechpracticetask/infrastructure/Controller.java
+++ src/main/java/me/petrolingus/infotechpracticetask/infrastructure/Controller.java
@@ -7,7 +7,17 @@ import javafx.scene.control.TextField;
 import me.petrolingus.infotechpracticetask.domain.Harmonic;
 import me.petrolingus.infotechpracticetask.domain.generator.DiscreteSignalGenerator;
 import me.petrolingus.infotechpracticetask.domain.generator.HarmonicGenerator;
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
+import org.apache.commons.math3.transform.DftNormalization;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -63,19 +73,89 @@ public class Controller {
                 samplingFrequency,
                 harmonics);
 
-        double[] signalArray = discreteSignalGenerator.generate();
+        double[] signalData = discreteSignalGenerator.generate();
 
-//        NoiseGenerator noiseGenerator = new NoiseGenerator(signalArray);
+        // TODO: 06.04.2021 Добавить генерацию шума
 
-//        double[] signalWithNoiseArray = noiseGenerator.generate();
-
-        XYChart.Series<Number, Number> signalSeries = new XYChart.Series<>(
-                FXCollections.observableList(IntStream.range(0, signalArray.length)
-                        .mapToObj(i -> new XYChart.Data<Number, Number>(i, signalArray[i]))
-                        .collect(Collectors.toList()))
-        );
-
+        XYChart.Series<Number, Number> signalSeries = arrayToSeries(signalData);
         signalChart.getData().clear();
         signalChart.getData().add(signalSeries);
+
+
+        double[] autocorrelation = new double[128];
+        for (int m = 0; m < autocorrelation.length; m++) {
+            double value = 0;
+            for (int i = 0; i < autocorrelation.length; i++) {
+                if (i + m >= signalData.length) {
+                    break;
+                }
+                value += signalData[i] * signalData[i + m];
+            }
+            autocorrelation[m] = value;
+        }
+        XYChart.Series<Number, Number> autocorrelationSeries = arrayToSeries(autocorrelation);
+        autocorrelationChart.getData().clear();
+        autocorrelationChart.getData().add(autocorrelationSeries);
+
+        FastFourierTransformer fastFourierTransformer = new FastFourierTransformer(DftNormalization.STANDARD);
+        Complex[] complex = fastFourierTransformer.transform(autocorrelation, TransformType.FORWARD);
+
+        double[] spectrum = new double[complex.length];
+        for (int i = 0; i < spectrum.length; i++) {
+            spectrum[i] = complex[i].abs();
+        }
+        XYChart.Series<Number, Number> spectrumSeries = arrayToSeries(spectrum);
+        spectrumChart.getData().clear();
+        spectrumChart.getData().add(spectrumSeries);
+
+        double[][] A1 = new double[autocorrelation.length][autocorrelation.length];
+        for (int i = 0; i < autocorrelation.length; i++) {
+            for (int j = 0; j < autocorrelation.length; j++) {
+                int index = Math.abs(i - j);
+                A1[i][j] = autocorrelation[index];
+            }
+        }
+
+        SingularValueDecomposition decomposition = new SingularValueDecomposition(MatrixUtils.createRealMatrix(A1));
+
+        double[] singularValues = decomposition.getSingularValues();
+//        System.out.println(Arrays.toString(singularValues));
+
+        // Находим максимальное сингулярное число
+        double maxSingularValue = singularValues[0];
+        for (double singularValue : singularValues) {
+            if (maxSingularValue < singularValue) {
+                maxSingularValue = singularValue;
+            }
+        }
+
+        // Обнуляем сигмы
+        for (int i = 0; i < singularValues.length; i++) {
+            double ratio = singularValues[i] / maxSingularValue;
+            if (ratio < 1e-3) {
+                singularValues[i] = 0;
+            }
+        }
+
+        RealMatrix U = decomposition.getU();
+        RealMatrix S = MatrixUtils.createRealDiagonalMatrix(singularValues);
+        RealMatrix V = decomposition.getV().transpose();
+
+        RealMatrix A2 = U.multiply(S).multiply(V);
+
+        double[] autocorrelation2 = new double[A2.getRowDimension()];
+        for (int i = 0; i < autocorrelation2.length; i++) {
+            autocorrelation2[i] = A2.getEntry(0, i);
+        }
+        XYChart.Series<Number, Number> autocorrelationSeries2 = arrayToSeries(autocorrelation2);
+        autocorrelationChart.getData().add(autocorrelationSeries2);
+    }
+
+    private XYChart.Series<Number, Number> arrayToSeries(double[] data) {
+        List<XYChart.Data<Number, Number>> list = new ArrayList<>();
+        for (int i = 0; i < data.length; i++) {
+            list.add(new XYChart.Data<>(i, data[i]));
+        }
+        return new XYChart.Series<>(FXCollections.observableList(list));
     }
 }
